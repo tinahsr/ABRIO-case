660a3faf29576fd74a48abdda160553f000e3178
Tina Haser <tinahaser@gmail.com>
2025-09-02 18:37:27 +0200

    cart-design

--- server/src/main.ts
+++ server/src/main.ts
@@ -11,6 +11,9 @@ async function bootstrap() {
       .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors({
+    host: 'localhost',
+  });
   await app.listen(process.env.PORT ?? 8080);
 }
 
